08972c772249adbdb164019400015c3ae4fbd343
Ichinose Shogo <shogo82148@gmail.com>
2022-09-07 23:24:34 +0900

    implement parsing JWT(RS256)

---
+++ jwk/rsa.go
@@ -0,0 +1,80 @@
+package jwk
+
+import (
+	"crypto/rsa"
+	"math/big"
+)
+
+func parseRSAKey(data *commonKey) (*Key, error) {
+	key, err := data.decode()
+	if err != nil {
+		return nil, err
+	}
+	ctx := newRSAContext(data)
+	var privateKey rsa.PrivateKey
+
+	// parameters for public key
+	var e int
+	for _, v := range ctx.decode(data.E, "e") {
+		e = (e << 8) | int(v)
+	}
+	privateKey.PublicKey.E = e
+	privateKey.PublicKey.N = new(big.Int).SetBytes(ctx.decode(data.N, "n"))
+	key.PublicKey = &privateKey.PublicKey
+
+	// parameters for private key
+	if data.D != "" {
+		d := new(big.Int).SetBytes(ctx.decode(data.D, "d"))
+		p := new(big.Int).SetBytes(ctx.decode(data.P, "p"))
+		q := new(big.Int).SetBytes(ctx.decode(data.Q, "q"))
+		privateKey.D = d
+		privateKey.Primes = []*big.Int{p, q}
+
+		key.PrivateKey = &privateKey
+	}
+
+	if ctx.err != nil {
+		return nil, ctx.err
+	}
+	return key, nil
+}
+
+func newRSAContext(key *commonKey) base64Context {
+	var size int
+	if len(key.E) > size {
+		size = len(key.E)
+	}
+	if len(key.N) > size {
+		size = len(key.N)
+	}
+	if len(key.D) > size {
+		size = len(key.D)
+	}
+	if len(key.P) > size {
+		size = len(key.P)
+	}
+	if len(key.Q) > size {
+		size = len(key.Q)
+	}
+	for _, v := range key.Oth {
+		if len(v.R) > size {
+			size = len(v.R)
+		}
+		if len(v.D) > size {
+			size = len(v.D)
+		}
+		if len(v.D) > size {
+			size = len(v.D)
+		}
+	}
+	if len(key.Dp) > size {
+		size = len(key.Dp)
+	}
+	if len(key.Dq) > size {
+		size = len(key.Dq)
+	}
+	if len(key.Qi) > size {
+		size = len(key.Qi)
+	}
+	return newBase64Context(size)
+}
