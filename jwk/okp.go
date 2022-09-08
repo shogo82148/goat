70f3061768ea3235f1e97ef19cc3ead4acd93d8c
Ichinose Shogo <shogo82148@gmail.com>
2022-09-09 00:28:59 +0900

    implement parsing Ed25519

---
+++ jwk/okp.go
@@ -0,0 +1,40 @@
+package jwk
+
+import (
+	"errors"
+	"fmt"
+)
+
+// RFC8037 2.  Key Type "OKP"
+func parseOKPKey(data *commonKey) (*Key, error) {
+	ctx := newOKPContext(data)
+	key, err := data.decode(ctx)
+	if err != nil {
+		return nil, err
+	}
+	switch data.Crv {
+	case "Ed25519":
+		return parseEd25519Key(ctx, data, key)
+	case "":
+		return nil, errors.New("jwk: the crv parameter is missing")
+	default:
+		return nil, fmt.Errorf("jwk: unknown crv: %q", data.Crv)
+	}
+}
+
+func newOKPContext(key *commonKey) *base64Context {
+	var size int
+	if len(key.X) > size {
+		size = len(key.X)
+	}
+	if len(key.D) > size {
+		size = len(key.D)
+	}
+	if len(key.X5t) > size {
+		size = len(key.X5t)
+	}
+	if len(key.X5tS256) > size {
+		size = len(key.X5tS256)
+	}
+	return newBase64Context(size)
+}
