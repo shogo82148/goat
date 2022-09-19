49e19ce67e750033c002a1b047824002bdb7e764
Ichinose Shogo <shogo82148@gmail.com>
2022-09-19 21:11:06 +0900

    add test for unsecured JWT

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -5,7 +5,8 @@ import (
 	"testing"
 	"time"
 
-	_ "github.com/shogo82148/goat/jwa/hs" // for HMAC SHA-256
+	_ "github.com/shogo82148/goat/jwa/hs"   // for HMAC SHA-256
+	_ "github.com/shogo82148/goat/jwa/none" // for none
 	"github.com/shogo82148/goat/jwk"
 	"github.com/shogo82148/goat/jws"
 	"github.com/shogo82148/goat/sig"
@@ -39,6 +40,34 @@ func TestParse(t *testing.T) {
 		token, err := Parse(context.TODO(), raw, jws.FindKeyFunc(func(ctx context.Context, header *jws.Header) (sig.Key, error) {
 			alg := header.Algorithm.New()
 			return alg.NewKey(key.PrivateKey, key.PublicKey), nil
+		}))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		c := token.Claims
+		if want, got := "joe", c.Issuer; want != got {
+			t.Errorf("invalid issuer: want %q, got %q", want, got)
+		}
+		if want, got := time.Unix(1300819380, 0), token.Claims.ExpirationTime; !want.Equal(got) {
+			t.Errorf("invalid exp claim: want %s, got %s", want, got)
+		}
+		if v, ok := c.Raw["http://example.com/is_root"].(bool); !ok || !v {
+			t.Error("unexpected claim")
+		}
+	})
+
+	t.Run("RFC7519 Section 6.1. Example Unsecured JWT", func(t *testing.T) {
+		raw := []byte(
+			"eyJhbGciOiJub25lIn0" +
+				"." +
+				"eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt" +
+				"cGxlLmNvbS9pc19yb290Ijp0cnVlfQ" +
+				".",
+		)
+		token, err := Parse(context.TODO(), raw, jws.FindKeyFunc(func(ctx context.Context, header *jws.Header) (sig.Key, error) {
+			alg := header.Algorithm.New()
+			return alg.NewKey(nil, nil), nil
 		}))
 		if err != nil {
 			t.Fatal(err)
