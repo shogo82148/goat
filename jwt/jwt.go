79403931d416ba9c26552e26e778b482b2b3ad3f
Ichinose Shogo <shogo82148@gmail.com>
2022-09-19 21:03:45 +0900

    implement jwt.Parse

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,6 +1,17 @@
 package jwt
 
-import "time"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/shogo82148/goat/internal/jsonutils"
+	"github.com/shogo82148/goat/jws"
+)
+
+var nowFunc = time.Now // for testing
 
 // Claims is a JWT Claims Set defined in RFC7519.
 type Claims struct {
@@ -27,4 +38,67 @@ type Claims struct {
 
 	// Raw is a JSON-decoded data of the claims.
 	Raw map[string]any
+}
+
+// Token is a decoded JWT token.
+type Token struct {
+	Header *jws.Header
+	Claims *Claims
+}
+
+func Parse(ctx context.Context, data []byte, finder jws.KeyFinder) (*Token, error) {
+	msg, err := jws.Parse(ctx, data, finder)
+	if err != nil {
+		return nil, err
+	}
+	c, err := parseClaims(msg.Payload)
+	if err != nil {
+		return nil, err
+	}
+	token := &Token{
+		Header: msg.Header,
+		Claims: c,
+	}
+	return token, nil
+}
+
+func parseClaims(data []byte) (*Claims, error) {
+	now := nowFunc()
+
+	var raw map[string]any
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.UseNumber()
+	if err := dec.Decode(&raw); err != nil {
+		return nil, fmt.Errorf("jwt: failed to parse claims: %w", err)
+	}
+	c := &Claims{
+		Raw: raw,
+	}
+	d := jsonutils.NewDecoder("jwt", raw)
+
+	c.Issuer, _ = d.GetString("iss")
+	c.Subject, _ = d.GetString("sub")
+	c.Audience, _ = d.GetString("aud")
+
+	if t, ok := d.GetTime("exp"); ok {
+		c.ExpirationTime = t
+		if !now.Before(t) {
+			d.NewError(fmt.Errorf("jwt: token is expired"))
+		}
+	}
+
+	if t, ok := d.GetTime("nbf"); ok {
+		c.NotBefore = t
+		if now.Before(t) {
+			d.NewError(fmt.Errorf("jwt: token is not valid yet"))
+		}
+	}
+
+	c.IssuedAt, _ = d.GetTime("iat")
+	c.JWTID, _ = d.GetString("jti")
+
+	if err := d.Err(); err != nil {
+		return nil, err
+	}
+	return c, nil
 }
---
+++ jwt/jwt_test.go
@@ -0,0 +1,58 @@
+package jwt
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	_ "github.com/shogo82148/goat/jwa/hs" // for HMAC SHA-256
+	"github.com/shogo82148/goat/jwk"
+	"github.com/shogo82148/goat/jws"
+	"github.com/shogo82148/goat/sig"
+)
+
+func TestParse(t *testing.T) {
+	nowFunc = func() time.Time {
+		return time.Unix(1300819379, 0)
+	}
+	defer func() {
+		nowFunc = time.Now
+	}()
+
+	t.Run("RFC7519 Section 3.1. Example JWT", func(t *testing.T) {
+		raw := []byte(
+			"eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9" +
+				"." +
+				"eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt" +
+				"cGxlLmNvbS9pc19yb290Ijp0cnVlfQ" +
+				"." +
+				"dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk",
+		)
+		rawKey := `{"kty":"oct",` +
+			`"k":"AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75` +
+			`aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"` +
+			`}`
+		key, err := jwk.ParseKey([]byte(rawKey))
+		if err != nil {
+			t.Fatal(err)
+		}
+		token, err := Parse(context.TODO(), raw, jws.FindKeyFunc(func(ctx context.Context, header *jws.Header) (sig.Key, error) {
+			alg := header.Algorithm.New()
+			return alg.NewKey(key.PrivateKey, key.PublicKey), nil
+		}))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		c := token.Claims
+		if want, got := "joe", c.Issuer; want != got {
+			t.Errorf("invalid issuer: want %q, got %q", want, got)
+		}
+		if want, got := time.Unix(1300819380, 0), token.Claims.ExpirationTime; !want.Equal(got) {
+			t.Errorf("invalid exp claim: want %s, got %s", want, got)
+		}
+		if v, ok := c.Raw["http://example.com/is_root"].(bool); !ok || !v {
+			t.Error("unexpected claim")
+		}
+	})
+}
