b35654d65e3fca005bb9962b61da86608667b9d9
Ichinose Shogo <shogo82148@gmail.com>
2022-09-19 16:57:05 +0900

    add test for RFC7515 Appendix A.4 Example JWS Using ECDSA P-521 SHA-512

--- jws/jws_test.go
+++ jws/jws_test.go
@@ -161,4 +161,47 @@ func TestParse(t *testing.T) {
 			t.Error("unexpected payload")
 		}
 	})
+
+	t.Run("RFC7515 Appendix A.4 Example JWS Using ECDSA P-521 SHA-512", func(t *testing.T) {
+		raw := []byte(
+			"eyJhbGciOiJFUzUxMiJ9" +
+				"." +
+				"UGF5bG9hZA" +
+				"." +
+				"AdwMgeerwtHoh-l192l60hp9wAHZFVJbLfD_UxMi70cwnZOYaRI1bKPWROc-mZZq" +
+				"wqT2SI-KGDKB34XO0aw_7XdtAG8GaSwFKdCAPZgoXD2YBJZCPEX3xKpRwcdOO8Kp" +
+				"EHwJjyqOgzDO7iKvU8vcnwNrmxYbSW9ERBXukOXolLzeO_Jn",
+		)
+		rawKey := `{"kty":"EC",` +
+			`"crv":"P-521",` +
+			`"x":"AekpBQ8ST8a8VcfVOTNl353vSrDCLLJXmPk06wTjxrrjcBpXp5EOnYG_` +
+			`NjFZ6OvLFV1jSfS9tsz4qUxcWceqwQGk",` +
+			`"y":"ADSmRA43Z1DSNx_RvcLI87cdL07l6jQyyBXMoxVg_l2Th-x3S1WDhjDl` +
+			`y79ajL4Kkd0AZMaZmh9ubmf63e3kyMj2",` +
+			`"d":"AY5pb7A0UFiB3RELSD64fTLOSV_jazdF7fLYyuTw8lOfRhWg6Y6rUrPA` +
+			`xerEzgdRhajnu0ferB0d53vM9mE15j2C"` +
+			`}`
+		key, err := jwk.ParseKey([]byte(rawKey))
+		if err != nil {
+			t.Fatal(err)
+		}
+		msg, err := Parse(context.TODO(), raw, FindKeyFunc(func(ctx context.Context, header *Header) (sig.Key, error) {
+			alg := header.Algorithm.New()
+			return alg.NewKey(key.PrivateKey, key.PublicKey), nil
+		}))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if want, got := msg.Header.Algorithm, jwa.ES512; want != got {
+			t.Errorf("unexpected algorithm: want %s, got %s", want, got)
+		}
+
+		payload := []byte(`{"iss":"joe",` +
+			`"exp":1300819380,` +
+			`"http://example.com/is_root":true}`)
+		if bytes.Equal(payload, msg.Payload) {
+			t.Error("unexpected payload")
+		}
+	})
 }
