5cb0dccd79fba5733328316e98309a04f7af61c3
Ichinose Shogo <shogo82148@gmail.com>
2022-09-20 14:51:43 +0900

    add test for encoding symmetric keys

--- jwk/jwk_test.go
+++ jwk/jwk_test.go
@@ -1,12 +1,12 @@
 package jwk
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/ed25519"
 	"crypto/elliptic"
 	"crypto/rsa"
 	"math/big"
-	"reflect"
 	"testing"
 
 	"github.com/shogo82148/goat/jwa"
@@ -202,7 +202,7 @@ func TestParseKey_RFC7517AppendixA(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if key.KeyType != "oct" {
+		if key.KeyType != jwa.Oct {
 			t.Errorf("unexpected key type: want %s, got %s", "oct", key.KeyType)
 		}
 		if key.Algorithm != "A128KW" {
@@ -216,7 +216,7 @@ func TestParseKey_RFC7517AppendixA(t *testing.T) {
 			0x19, 0xac, 0x20, 0x82, 0xe1, 0x72, 0x1a, 0xb5,
 			0x8a, 0x6a, 0xfe, 0xc0, 0x5f, 0x85, 0x4a, 0x52,
 		}
-		if !reflect.DeepEqual(want, got) {
+		if !bytes.Equal(want, got) {
 			t.Errorf("unexpected key value: want %x, got %x", want, got)
 		}
 	})
@@ -227,7 +227,7 @@ func TestParseKey_RFC7517AppendixA(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if key.KeyType != "oct" {
+		if key.KeyType != jwa.Oct {
 			t.Errorf("unexpected key type: want %s, got %s", "oct", key.KeyType)
 		}
 		got, ok := key.PrivateKey.([]byte)
@@ -244,7 +244,7 @@ func TestParseKey_RFC7517AppendixA(t *testing.T) {
 			0x3d, 0x2e, 0x21, 0x72, 0x05, 0x2e, 0x4f, 0x08,
 			0xc0, 0xcd, 0x9a, 0xf5, 0x67, 0xd0, 0x80, 0xa3,
 		}
-		if !reflect.DeepEqual(want, got) {
+		if !bytes.Equal(want, got) {
 			t.Errorf("unexpected key value: want %x, got %x", want, got)
 		}
 	})
@@ -791,6 +791,48 @@ func TestMarshalKey_RFC7517AppendixA(t *testing.T) {
 			`"qi":"GyM_p6JrXySiz1toFgKbWV-JdI3jQ4ypu9rbMWx3rQJBfmt0FoYzg` +
 			`UIZEVFEcOqwemRN81zoDAaa-Bk0KWNGDjJHZDdDmFhW3AN7lI-puxk_mHZGJ11rx` +
 			`yR8O55XLSe3SPmRfKwZI6yU24ZxvQKFYItdldUKGzO6Ia6zTKhAVRU"}`
+		if want != string(got) {
+			t.Errorf("unexpected JWK: want %s, got %s", want, got)
+		}
+	})
+
+	t.Run("RFC 7517 A.3. Example Symmetric Keys (A128KW)", func(t *testing.T) {
+		key := &Key{
+			Algorithm: jwa.A128KW.KeyAlgorithm(),
+			PrivateKey: []byte{
+				0x19, 0xac, 0x20, 0x82, 0xe1, 0x72, 0x1a, 0xb5,
+				0x8a, 0x6a, 0xfe, 0xc0, 0x5f, 0x85, 0x4a, 0x52,
+			},
+		}
+		got, err := key.MarshalJSON()
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := `{"alg":"A128KW","k":"GawgguFyGrWKav7AX4VKUg","kty":"oct"}`
+		if want != string(got) {
+			t.Errorf("unexpected JWK: want %s, got %s", want, got)
+		}
+	})
+
+	t.Run("RFC 7517 A.3. Example Symmetric Keys (HMAC)", func(t *testing.T) {
+		key := &Key{
+			KeyID: "HMAC key used in JWS spec Appendix A.1 example",
+			PrivateKey: []byte{
+				0x03, 0x23, 0x35, 0x4b, 0x2b, 0x0f, 0xa5, 0xbc,
+				0x83, 0x7e, 0x06, 0x65, 0x77, 0x7b, 0xa6, 0x8f,
+				0x5a, 0xb3, 0x28, 0xe6, 0xf0, 0x54, 0xc9, 0x28,
+				0xa9, 0x0f, 0x84, 0xb2, 0xd2, 0x50, 0x2e, 0xbf,
+				0xd3, 0xfb, 0x5a, 0x92, 0xd2, 0x06, 0x47, 0xef,
+				0x96, 0x8a, 0xb4, 0xc3, 0x77, 0x62, 0x3d, 0x22,
+				0x3d, 0x2e, 0x21, 0x72, 0x05, 0x2e, 0x4f, 0x08,
+				0xc0, 0xcd, 0x9a, 0xf5, 0x67, 0xd0, 0x80, 0xa3,
+			},
+		}
+		got, err := key.MarshalJSON()
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := `{"k":"AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow","kid":"HMAC key used in JWS spec Appendix A.1 example","kty":"oct"}`
 		if want != string(got) {
 			t.Errorf("unexpected JWK: want %s, got %s", want, got)
 		}
