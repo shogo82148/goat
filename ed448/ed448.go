2df64319b001e5b4b73405b3186a84dcbf95fd8a
ICHINOSE Shogo <shogo82148@gmail.com>
2022-10-24 00:59:31 +0900

    basic implement of Scalar

---
+++ ed448/ed448.go
@@ -0,0 +1,103 @@
+package ed448
+
+import (
+	"bytes"
+	"crypto"
+	cryptorand "crypto/rand"
+	"io"
+	"strconv"
+
+	"golang.org/x/crypto/sha3"
+)
+
+const (
+	// PublicKeySize is the size, in bytes, of public keys as used in this package.
+	PublicKeySize = 57
+	// PrivateKeySize is the size, in bytes, of private keys as used in this package.
+	PrivateKeySize = 114
+	// SignatureSize is the size, in bytes, of signatures generated and verified by this package.
+	SignatureSize = 114
+	// SeedSize is the size, in bytes, of private key seeds. These are the private key representations used by RFC 8032.
+	SeedSize = 57
+)
+
+// PublicKey is the type of Ed448 public keys.
+type PublicKey []byte
+
+// Equal reports whether pub and x have the same value.
+func (pub PublicKey) Equal(x crypto.PublicKey) bool {
+	xx, ok := x.(PublicKey)
+	if !ok {
+		return false
+	}
+	return bytes.Equal(pub, xx)
+}
+
+// PrivateKey is the type of X448 private keys.
+type PrivateKey []byte
+
+// Public returns the PublicKey corresponding to priv.
+func (priv PrivateKey) Public() crypto.PublicKey {
+	publicKey := make([]byte, PublicKeySize)
+	copy(publicKey, priv[57:])
+	return PublicKey(publicKey)
+}
+
+// Equal reports whether priv and x have the same value.
+func (priv PrivateKey) Equal(x crypto.PrivateKey) bool {
+	xx, ok := x.(PrivateKey)
+	if !ok {
+		return false
+	}
+	return bytes.Equal(priv, xx)
+}
+
+// Seed returns the private key seed corresponding to priv. It is provided for
+// interoperability with RFC 8032. RFC 8032's private keys correspond to seeds
+// in this package.
+func (priv PrivateKey) Seed() []byte {
+	seed := make([]byte, SeedSize)
+	copy(seed, priv[:57])
+	return seed
+}
+
+// GenerateKey generates a public/private key pair using entropy from rand.
+// If rand is nil, crypto/rand.Reader will be used.
+func GenerateKey(rand io.Reader) (PublicKey, PrivateKey, error) {
+	if rand == nil {
+		rand = cryptorand.Reader
+	}
+
+	seed := make([]byte, SeedSize)
+	if _, err := io.ReadFull(rand, seed); err != nil {
+		return nil, nil, err
+	}
+
+	privateKey := make([]byte, PrivateKeySize)
+	newKeyFromSeed(privateKey, seed)
+
+	publicKey := make([]byte, PublicKeySize)
+	copy(publicKey, privateKey[57:])
+
+	return publicKey, privateKey, nil
+}
+
+// NewKeyFromSeed calculates a private key from a seed. It will panic if
+// len(seed) is not SeedSize. This function is provided for interoperability
+// with RFC 8032. RFC 8032's private keys correspond to seeds in this
+// package.
+func NewKeyFromSeed(seed []byte) PrivateKey {
+	privateKey := make([]byte, PrivateKeySize)
+	newKeyFromSeed(privateKey, seed)
+	return privateKey
+}
+
+func newKeyFromSeed(privateKey, seed []byte) {
+	if l := len(seed); l != SeedSize {
+		panic("x448: bad seed length: " + strconv.Itoa(l))
+	}
+
+	h := make([]byte, 114)
+	sha3.ShakeSum256(h, seed)
+	_ = h[57:]
+}
---
+++ internal/edwards448/scalar.go
@@ -0,0 +1,77 @@
+// Copyright (c) 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package edwards448
+
+import "math/big"
+
+// A Scalar is an integer modulo
+//
+//	l = 2^446 - 13818066809895115352007386748515426880336692474882178609894547503885
+//
+// which is the prime order of the edwards25519 group.
+//
+// This type works similarly to math/big.Int, and all arguments and
+// receivers are allowed to alias.
+//
+// The zero value is a valid zero element.
+type Scalar struct {
+	v *big.Int
+}
+
+func NewScalar() *Scalar {
+	return &Scalar{new(big.Int)}
+}
+
+var l, _ = new(big.Int).SetString("181709681073901722637330951972001133588410340171829515070372549795146003961539585716195755291692375963310293709091662304773755859649779", 10)
+
+func (s *Scalar) MulAdd(x, y, z *Scalar) *Scalar {
+	// TODO: reimplement with constant-time algorithm
+	s.v.Mul(x.v, y.v)
+	s.v.Add(s.v, z.v)
+	s.v.Mod(s.v, l)
+	return s
+}
+
+func (s *Scalar) Add(x, y *Scalar) *Scalar {
+	// TODO: reimplement with constant-time algorithm
+	s.v.Add(x.v, y.v)
+	s.v.Mod(s.v, l)
+	return s
+}
+
+func (s *Scalar) Sub(x, y *Scalar) *Scalar {
+	// TODO: reimplement with constant-time algorithm
+	s.v.Add(x.v, l)
+	s.v.Sub(s.v, y.v)
+	s.v.Mod(s.v, l)
+	return s
+}
+
+func (s *Scalar) Negate(x *Scalar) *Scalar {
+	// TODO: reimplement with constant-time algorithm
+	s.v.Sub(l, x.v)
+	s.v.Mod(s.v, l)
+	return s
+}
+
+func (s *Scalar) Mul(x, y *Scalar) *Scalar {
+	// TODO: reimplement with constant-time algorithm
+	s.v.Mul(x.v, y.v)
+	s.v.Mod(s.v, l)
+	return s
+}
+
+func (s *Scalar) Set(x *Scalar) *Scalar {
+	s.v.Set(x.v)
+	return s
+}
+
+func (s *Scalar) Equal(t *Scalar) int {
+	// TODO: reimplement with constant-time algorithm
+	if s.v.Cmp(t.v) == 0 {
+		return 1
+	}
+	return 0
+}
