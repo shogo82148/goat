5c30e48629865e790501164c961746abc6636a99
ICHINOSE Shogo <shogo82148@gmail.com>
2022-10-28 00:12:44 +0900

    add benchmarks for Sign and Verify

--- ed448/ed448_test.go
+++ ed448/ed448_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func decodeHex(t *testing.T, s string) []byte {
+func decodeHex(t testing.TB, s string) []byte {
 	t.Helper()
 	data, err := hex.DecodeString(s)
 	if err != nil {
@@ -290,5 +290,40 @@ func TestSignAndVerify(t *testing.T) {
 				t.Error("verification failed")
 			}
 		})
+	}
+}
+
+func BenchmarkSign(b *testing.B) {
+	seed := decodeHex(b, "6c82a562cb808d10d632be89c8513ebf"+
+		"6c929f34ddfa8c9f63c9960ef6e348a3"+
+		"528c8a3fcc2f044e39a3fc5b94492f8f"+
+		"032e7549a20098f95b")
+	message := []byte{}
+	priv := NewKeyFromSeed(seed)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Sign(priv, message)
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	pub := decodeHex(b, "5fd7449b59b461fd2ce787ec616ad46a"+
+		"1da1342485a70e1f8a0ea75d80e96778"+
+		"edf124769b46c7061bd6783df1e50f6c"+
+		"d1fa1abeafe8256180")
+	message := []byte{}
+	signature := decodeHex(b, "533a37f6bbe457251f023c0d88f976ae"+
+		"2dfb504a843e34d2074fd823d41a591f"+
+		"2b233f034f628281f2fd7a22ddd47d78"+
+		"28c59bd0a21bfd3980ff0d2028d4b18a"+
+		"9df63e006c5d1c2d345b925d8dc00b41"+
+		"04852db99ac5c7cdda8530a113a0f4db"+
+		"b61149f05a7363268c71d95808ff2e65"+
+		"2600")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Verify(PublicKey(pub), message, signature)
 	}
 }
