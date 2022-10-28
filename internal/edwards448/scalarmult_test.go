754f9d3ac1fe88adcadc68c1f6b22bc3464e3fd1
Ichinose Shogo <shogo82148@gmail.com>
2022-10-28 15:58:03 +0900

    add test for ScalarMult

--- internal/edwards448/scalarmult_test.go
+++ internal/edwards448/scalarmult_test.go
@@ -50,6 +50,36 @@ func BenchmarkScalarBaseMultMinus1(b *testing.B) {
 	}
 }
 
+func TestScalarMult(t *testing.T) {
+	t.Run("positive", func(t *testing.T) {
+		s := new(Scalar)
+		g := NewGeneratorPoint()
+		want := new(Point).Zero()
+		for i := 0; i <= 1024; i++ {
+			got := new(Point).ScalarMult(s, g)
+			if got.Equal(want) == 0 {
+				t.Errorf("%d: got %#v, want %#v", i, got.Bytes(), want.Bytes())
+			}
+			s.Add(s, &scOne)
+			want.Add(want, g)
+		}
+	})
+
+	t.Run("negative", func(t *testing.T) {
+		s := new(Scalar)
+		g := NewGeneratorPoint()
+		want := new(Point).Zero()
+		for i := 0; i <= 1024; i++ {
+			got := new(Point).ScalarMult(s, g)
+			if got.Equal(want) == 0 {
+				t.Errorf("%d, got %#v, want %#v", -i, got.Bytes(), want.Bytes())
+			}
+			s.Add(s, &scMinusOne)
+			want.Sub(want, g)
+		}
+	})
+}
+
 func BenchmarkScalarMult1(b *testing.B) {
 	s := new(Scalar).Set(&scOne)
 	p := NewGeneratorPoint()
