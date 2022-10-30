ffd1c3f625afcc7b394bb620f66d025169f9db16
ICHINOSE Shogo <shogo82148@gmail.com>
2022-10-31 06:58:12 +0900

    error check

--- secp256k1/secp256k1.go
+++ secp256k1/secp256k1.go
@@ -178,8 +178,12 @@ func (crv *secp256k1) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
 
 	var p1, p2, p3 curve256k1.Point
 	var pj1, pj2, pj3 curve256k1.PointJacobian
-	p1.NewPoint(x1, y1)
-	p2.NewPoint(x2, y2)
+	if _, err := p1.NewPoint(x1, y1); err != nil {
+		panic("invalid point")
+	}
+	if _, err := p2.NewPoint(x2, y2); err != nil {
+		panic("invalid point")
+	}
 	pj1.FromAffine(&p1)
 	pj2.FromAffine(&p2)
 	pj3.Add(&pj1, &pj2)
