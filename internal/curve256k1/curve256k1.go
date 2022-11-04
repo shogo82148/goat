74f9a436a23700bdc008b116849a9af2a847087e
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-04 19:47:14 +0900

    fix out of range panic

--- internal/curve256k1/curve256k1.go
+++ internal/curve256k1/curve256k1.go
@@ -276,12 +276,14 @@ func (p *PointJacobian) ScalarMult(q *PointJacobian, k []byte) *PointJacobian {
 	var zero PointJacobian
 	zero.Zero()
 	p.Zero()
-	for i := 0; i < 256; i++ {
-		var tmp PointJacobian
-		b := int(k[i/8]>>(7-i%8)) & 1
-		p.Double(p)
-		tmp.Select(q, &zero, b)
-		p.Add(p, &tmp)
+	for i := 0; i < len(k); i++ {
+		b := int(k[i])
+		for j := 7; j >= 0; j-- {
+			var tmp PointJacobian
+			p.Double(p)
+			tmp.Select(q, &zero, (b>>j)&1)
+			p.Add(p, &tmp)
+		}
 	}
 	return p
 }
