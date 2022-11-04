fc840c49d8125415cf28a90f31afe3fb99131a1a
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-04 20:57:21 +0900

    add benchmark for ScalarMult

--- internal/curve256k1/scalarmult_test.go
+++ internal/curve256k1/scalarmult_test.go
@@ -2,7 +2,6 @@ package curve256k1
 
 import (
 	"bytes"
-	"encoding/hex"
 	"testing"
 )
 
@@ -11,7 +10,7 @@ func TestScalarMult1(t *testing.T) {
 	q.x.Set(hex2element("79b1031b16eaed727f951f0fadeebc9a950092861fe266869a2e57e6eda95a14"))
 	q.y.Set(hex2element("d39752c01275ea9b61c67990069243c158373d754a54b9acd2e8e6c5db677fbb"))
 	q.z.Set(hex2element("0000000000000000000000000000000000000000000000000000000000000001"))
-	k, _ := hex.DecodeString("0000000000000000000000000000000000000000000000000000000000000001")
+	k := decodeHex("0000000000000000000000000000000000000000000000000000000000000001")
 
 	q.ScalarMult(&q, k)
 
@@ -30,7 +29,7 @@ func TestScalarMinus1(t *testing.T) {
 	q.x.Set(hex2element("79b1031b16eaed727f951f0fadeebc9a950092861fe266869a2e57e6eda95a14"))
 	q.y.Set(hex2element("d39752c01275ea9b61c67990069243c158373d754a54b9acd2e8e6c5db677fbb"))
 	q.z.Set(hex2element("0000000000000000000000000000000000000000000000000000000000000001"))
-	k, _ := hex.DecodeString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140")
+	k := decodeHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140")
 
 	q.ScalarMult(&q, k)
 
@@ -41,5 +40,31 @@ func TestScalarMinus1(t *testing.T) {
 	}
 	if y := q.y.Bytes(); bytes.Equal(y, wantY) {
 		t.Errorf("want %x, got %x", wantY, y)
+	}
+}
+
+func BenchmarkScalarMult1(b *testing.B) {
+	var q PointJacobian
+	q.x.Set(hex2element("79b1031b16eaed727f951f0fadeebc9a950092861fe266869a2e57e6eda95a14"))
+	q.y.Set(hex2element("d39752c01275ea9b61c67990069243c158373d754a54b9acd2e8e6c5db677fbb"))
+	q.z.Set(hex2element("0000000000000000000000000000000000000000000000000000000000000001"))
+	k := decodeHex("0000000000000000000000000000000000000000000000000000000000000001")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		q.ScalarMult(&q, k)
+	}
+}
+
+func BenchmarkScalarMultMinus1(b *testing.B) {
+	var q PointJacobian
+	q.x.Set(hex2element("79b1031b16eaed727f951f0fadeebc9a950092861fe266869a2e57e6eda95a14"))
+	q.y.Set(hex2element("d39752c01275ea9b61c67990069243c158373d754a54b9acd2e8e6c5db677fbb"))
+	q.z.Set(hex2element("0000000000000000000000000000000000000000000000000000000000000001"))
+	k := decodeHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		q.ScalarMult(&q, k)
 	}
 }
