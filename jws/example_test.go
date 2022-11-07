1a3bb3fba2ea8f36f685d9ad9c698808c12da612
Ichinose Shogo <shogo82148@gmail.com>
2022-11-07 09:39:08 +0900

    add example test for Message.Compact

--- jws/example_test.go
+++ jws/example_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/shogo82148/goat/jwa"
 	"github.com/shogo82148/goat/jwk"
 	"github.com/shogo82148/goat/jws"
 	"github.com/shogo82148/goat/sig"
@@ -69,4 +70,28 @@ func ExampleMessage_Verify() {
 	fmt.Println(string(payload))
 	// Output:
 	// Example of Ed25519 signing
+}
+
+func ExampleMessage_Compact() {
+	rawKey := `{"kty":"OKP","crv":"Ed25519",` +
+		`"d":"nWGxne_9WmC6hEr0kuwsxERJxWl7MmkZcDusAxyuf2A",` +
+		`"x":"11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo"}`
+	key, err := jwk.ParseKey([]byte(rawKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	header := jws.NewHeader()
+	header.SetAlgorithm(jwa.EdDSA)
+	msg := jws.NewMessage([]byte("Example of Ed25519 signing"))
+	if err := msg.Sign(header, nil, jwa.EdDSA.New().NewSigningKey(key)); err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := msg.Compact()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+	// Output:
+	// eyJhbGciOiJFZERTQSJ9.RXhhbXBsZSBvZiBFZDI1NTE5IHNpZ25pbmc.hgyY0il_MGCjP0JzlnLWG1PPOt7-09PGcvMg3AIbQR6dWbhijcNR4ki4iylGjg5BhVsPt9g7sVvpAr_MuM0KAg
 }
