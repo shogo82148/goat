2278b87e47b02d053399059403491d0759ba04bf
ICHINOSE Shogo <shogo82148@gmail.com>
2023-07-31 20:38:56 +0900

    refer RFC 9110 Section 11.1. Authentication Scheme

--- oauth2/oauth2.go
+++ oauth2/oauth2.go
@@ -13,10 +13,11 @@ func ExtractBearer(req *http.Request) (bearer string, ok bool) {
 		return "", false
 	}
 
-	// RFC 6750 says "Unless otherwise noted, all the protocol
-	// parameter names and values are case sensitive.",
-	// so "Bearer" should be case sensitive.
-	// However some clients sends "bearer", and we accept them.
+	// RFC 9110 Section 11.1. Authentication Scheme:
+	// > It uses a case-insensitive token to identify the authentication scheme:
+	// >
+	// >     auth-scheme    = token
+	// >
 	if !strings.EqualFold(credentials[:len(prefix)], prefix) {
 		return "", false
 	}
